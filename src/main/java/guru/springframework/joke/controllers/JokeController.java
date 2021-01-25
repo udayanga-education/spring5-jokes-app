53915b7e389ec92c84dacdf34c9b5efe3a99c5f3
u-senanayake <mailtoudayanga@gmail.com>
2021-01-25 23:16:05 +0900

    Corrected template folder

--- src/main/java/guru/springframework/joke/controllers/JokeController.java
+++ src/main/java/guru/springframework/joke/controllers/JokeController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JokeController {
 
-    private JokeService jokeService;
+    private final JokeService jokeService;
 
     @Autowired
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"", ""})
+    @RequestMapping({"/", ""})
     public String showJoke(Model model) {
         model.addAttribute("joke", jokeService.getJoke());
         return "chucknoris";
